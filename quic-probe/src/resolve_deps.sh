d4339d11f87a275ae19d69293c72265387769a55
janev94 <misho.janew@gmail.com>
2019-03-14 13:24:59 +0000

    Prefering ipV4

--- quic-probe/src/resolve_deps.sh
+++ quic-probe/src/resolve_deps.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "Setting IPv4 preference"
+sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf
+
 echo 'Resolving dependencies'
 apt-get install git -y
 mkdir setup
