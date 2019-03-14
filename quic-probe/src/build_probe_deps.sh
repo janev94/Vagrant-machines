f5d67ac62b22ccb54e60f502db62d1bd592cd982
janev94 <misho.janew@gmail.com>
2019-03-14 23:46:04 +0000

    Added new provision scripts

---
+++ quic-probe/src/build_probe_deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Copying files'
+
+cp /home/vagrant/src/probe_servers.py /root/go/src/github.com/Eichhoernchen/quic-grabber/probe_servers.py
+cp /home/vagrant/src/extract_versions.py /root/go/src/github.com/Eichhoernchen/quic-grabber/extract_versions.py
+
+
+cd /root/go/src/github.com/Eichhoernchen/quic-grabber/
+wget https://gist.githubusercontent.com/janev94/29e9b8c9d20d463be0c43376583626e1/raw/78f6dbf2fabbf30d958b935a5f13c471befca1dd/servers_feb
+
+cp /home/vagrant/src/Makefile ./Makefile
+
+echo 'Done'
